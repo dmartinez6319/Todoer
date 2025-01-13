e4653a35c86cbcb7276e919339f2fc17a652c48c
Daniel Martinez <dmartinez6319@gmail.com>
2025-01-13 02:36:30 -0500

    main template html/css

--- src/index.js
+++ src/index.js
@@ -1,47 +1,14 @@
 import "./styles.css";
 
-const names = ["james","frank","henry"];
-const nums = ["1","2","3"];
+const button = document.querySelector("#task-status");
+const bigButton = document.querySelector("#task-item");
 
+button.addEventListener("click",(event)=>{
+    event.stopPropagation();
+    alert("touch")
+})
 
-const [j,,h] = names // extra commas skip an index
+bigButton.addEventListener("click", ()=>{
+    alert("wham")
+})
 
-console.log(j);
-console.log(h);
-
-const person = {
-    firstName: "john",
-    lastName: "smith",
-    money: 3
-}
-
-const {lastName, ...y} = person
-console.log(y);
-
-const swimmer = ({name}) => {
-    const swim = () => {
-        console.log(`${name} swam through smth`);
-    }
-    return {swim}
-}
-
-function flyer({name}) {
-    return {
-        fly: () => console.log(`${name} flew through smth`)
-        
-    }
-}
-
-const monsterCreator = (name) => {
-    const monster = {name: name};
-
-    return {
-        ...monster,
-        ...flyer(monster),
-        ...swimmer(monster)
-    };
-}
-
-const killdude = monsterCreator("jeff");
-console.log()
-killdude.swim()
